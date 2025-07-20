8c1db02d8ba2b22d431bfd2e8545a66a52e20237
Bruno Barreto <bruno-kk@hotmail.com>
2025-07-20 01:29:08 -0300

    parser+dbdump added

--- __test__/initDbDev.js
+++ __test__/initDbDev.js
@@ -1,11 +1,18 @@
 import mongoose from 'mongoose'
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
 import Book from '../models/book.js'
 import Request from '../models/request.js'
 import User from '../models/user.js'
 import Verse from '../models/verse.js'
-import books from './mock/books.json'
-import verses from './mock/verses.json'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const books = JSON.parse(fs.readFileSync(path.join(__dirname, './mock/complete_books.json'), 'utf8'))
+const verses = JSON.parse(fs.readFileSync(path.join(__dirname, './mock/parsed_complete_books.json'), 'utf8'))
 
 export const connect = async () => {
   return mongoose.connect('mongodb://localhost/abibliadigital', {
@@ -23,17 +30,38 @@ const createBooks = async () => {
 const createVerses = async (books) => {
   const bookByKey = {}
   await books.map(book => {
-    bookByKey[book.abbrev.en] = book
+    bookByKey[book.abbrev.pt] = book
   })
 
+  // Mapeamento para corrigir inconsistências entre versículos e livros
+  const abbrevMapping = {
+    'atos': 'at'
+  }
+
   const promises = verses.map(async verse => {
+    let bookKey = verse.book.abbrev.pt
+    
+    // Aplicar mapeamento se necessário
+    if (abbrevMapping[bookKey]) {
+      bookKey = abbrevMapping[bookKey]
+    }
+    
+    if (!bookByKey[bookKey]) {
+      console.log(`Livro não encontrado para abreviação: ${verse.book.abbrev.pt} (mapeado para: ${bookKey})`)
+      return null
+    }
+    
     verse.book = {
-      id: bookByKey[verse.book.abbrev.en]._id,
-      abbrev: bookByKey[verse.book.abbrev.en].abbrev
+      id: bookByKey[bookKey]._id,
+      abbrev: bookByKey[bookKey].abbrev
     }
     return new Verse(verse).save()
   })
-  return Promise.all(promises)
+  
+  const results = await Promise.all(promises)
+  const validResults = results.filter(result => result !== null)
+  console.log(`Versículos processados: ${validResults.length} de ${verses.length}`)
+  return validResults
 }
 
 const createUser = async () => {
